f3b9356050bde98d664b9ed08e2306f80071fcb6
1 1 <bobra2@ya.ru>
2022-08-23 08:54:44 +0300

    task 2 added

---
+++ task2/Program.cs
@@ -0,0 +1,36 @@
+﻿// Напишите цифру, которая выводит случайное число из отрезка [10:99] и показывает наибольшую цифру числа
+//78-->8
+//12-->2
+//85-->8
+using System;
+
+namespace Seminar2
+{
+class Program
+{
+static void Main (string[] args)
+{
+    Console.WriteLine("Hello world!");
+}
+        static  int MaxNumber()
+        {
+            int number1 = new Random().Next(10,1000);
+
+            int max = 0;
+
+            int iterator = number1;
+
+            while(iterator !=0)
+            {
+                int number2=iterator%10;
+
+                if(number2>max) max=number2;
+
+                iterator/=10;
+                
+                
+            }
+        }
+
+}
+}
