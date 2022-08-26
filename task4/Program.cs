d5a6378169103ae2fdcf3ce488491bca11738a0b
1 1 <bobra2@ya.ru>
2022-08-26 09:02:26 +0300

    task 4 and 5 added

---
+++ task4/Program.cs
@@ -0,0 +1,8 @@
+﻿//Console.WriteLine(new Random().Next(1,999));
+//убирает среднюю цифру из трехзначного числа
+int number=new Random().Next(100,999);
+Console.WriteLine(number);
+int number1=number/100;
+int number2=number%10;
+int number3=number1*10+number2;
+Console.WriteLine(number3);
---
+++ task5/Program.cs
@@ -0,0 +1,17 @@
+﻿//принимает на вход два числа и выводить, является ли второе число кратно первому. Если число 2 не кратно числу 1, то программа выводит остаток от деления
+Console.WriteLine("number1");
+//int number1=Convert.ToInt32(Console.ReadLine());
+int number1=int.Parse(Console.ReadLine());
+Console.WriteLine("number2");
+int number2=Convert.ToInt32(Console.ReadLine());
+
+if(number1%number2==0){
+    Console.WriteLine($"Число {number2} кратно {number1}");
+}
+else
+{
+    Console.WriteLine($"Число {number2} не кратно {number1}");
+     Console.WriteLine($"Остаток от деления {number1%number2}");
+}
+
+
